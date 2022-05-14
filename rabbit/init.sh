6d39dc99e2460c31c49c2458623d9bd153318928
Ilya Fateev <ifateev@spbtv.com>
2022-05-14 19:21:42 +0500

    trying to fix shit

--- rabbit/init.sh
+++ rabbit/init.sh
@@ -3,6 +3,7 @@
 # Create Rabbitmq user
 ( sleep 5 ; \
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
+rabbitmqctl start_app ; \
 rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
 rabbitmqctl set_permissions -p "/" $RABBITMQ_USER  ".*" ".*" ".*" ; \
 echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
